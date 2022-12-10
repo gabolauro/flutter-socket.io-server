2887aa1063e7e5a60b9bd45b8cdfafb83867b2ae
Gabriel Laurito <gabriel@photocert.co.uk>
2022-12-10 16:01:04 +0100

    Cambios sobre bandas

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,8 +1,36 @@
 const { io } = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+
+bands.addBand( new Band( 'Queen' ) );
+bands.addBand( new Band( 'Heroes del silencio' ) );
+bands.addBand( new Band( 'Metalica' ) );
+bands.addBand( new Band( 'Bon jovi' ) );
 
 // Sockets messages
 io.on('connection', client => {
-    console.log('Cliente conectado');
+   console.log('Cliente conectado');
+
+   client.emit('active-bands', bands.getBands())
+
+   client.on('vote-band', (payload) => {
+      bands.voteBand(payload.id);
+      io.emit('active-bands', bands.getBands())
+   })
+
+   client.on('add-band', (payload) => {
+      console.log('Recibido', payload);
+      bands.addBand( new Band( payload.name ) )
+      io.emit('active-bands', bands.getBands())
+   })
+
+   client.on('remove-band', (payload) => {
+      console.log('Recibido', payload);
+      bands.deleteBand( payload.id )
+      io.emit('active-bands', bands.getBands())
+   })
 
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
@@ -12,6 +40,12 @@ io.on('connection', client => {
         console.log('Mensaje', payload);
 
         io.emit( 'mensaje', { admin: 'Nuevo mensaje' } )
+     });
+
+     client.on('emitir-mensaje', (payload) => {
+        // io.emit('nuevo-mensaje', payload)
+      //   console.log('Recibido', payload);
+        client.broadcast.emit('nuevo-mensaje', payload)
      })
 
 });
